86d72a36ac42ad11323eab07a8d652303b663b98
Vasiliy Nikiforov <kaktusv6@gmail.com>
2022-04-21 23:07:48 +1000

    Add vk users

---
+++ database/migrations/2022_04_20_125510_create_vk_users.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('vk_users', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->timestamps();
+
+            $table->unique(['user_id']);
+            $table->index(['user_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('vk_users');
+    }
+};
